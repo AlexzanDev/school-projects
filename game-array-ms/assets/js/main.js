c456f4664223eeb5397c74b094a8f3a7a2588d83
AlexzanDev <alessandro@mzit.it>
2022-01-29 14:25:43 +0100

    Added second part.

--- game-array-ms/assets/js/main.js
+++ game-array-ms/assets/js/main.js
@@ -1,9 +1,12 @@
 console.log("Ready.");
 var timings = []; // Create array
+var subTimings = [];
 var cont = 0;
 var i = 0;
 var str = "";
-var totale = document.getElementById('totale');
+var strDiff = "";
+const totale = document.getElementById('totale');
+const differenze = document.getElementById('differenze');
 
 // Count time
 var start = Date.now(); // Get system date
@@ -11,11 +14,23 @@ function count() {
     timings[cont] = (Date.now() - start);
     document.getElementById("out").innerHTML="Sono passati " + (timings[cont]) + " millisecondi dal click.";
     cont++;
+    for(i=1;i<cont;i++) {
+        subTimings[i] = (timings[i] - timings[i-1]);
+        console.log(subTimings[i]);
+    }
+    console.log('Il contatore è a ' + cont);
 }
 
 function printTimings() {
     for(i=0;i<cont;i++) {
         str = str + " " + timings[i] + "ms";
         totale.innerHTML = 'Tutti i tempi: ' + str;
+    }
+}
+
+function printSubTimings() {
+    for(i=1;i<cont;i++) {
+        strDiff = strDiff + " " + subTimings[i] + "ms";
+        differenze.innerHTML = 'Differenze tra i tempi: ' + strDiff;
     }
 }
